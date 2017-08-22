974cd878f38ffc692fb3536e072c8b31a242db11
cylau1031 <cylau1031@gmail.com>
2017-08-22 18:26:58 -0400

    finished single album route part 5

--- browser/react/components/AllAlbums.js
+++ browser/react/components/AllAlbums.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class AllAlbums extends Component {
+  constructor() {
+    super()
+    this.state = {
+      albums: []
+
+    }
+  }
+
+  componentDidMount () {
+    axios.get('/api/albums/')
+      .then(res => res.data)
+      .then(albums => {
+        this.setState({ albums })
+      });
+  }
 
   render () {
-    const albums = this.props.albums;
-    const selectAlbum = this.props.selectAlbum;
+    console.log(this.props)
+    const albums = this.state.albums;
+   // const selectAlbum = this.props.selectAlbum;
 
     return (
       <div>
--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,40 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
 import Player from './Player';
+import {HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
 
 export default class Main extends Component {
 
   constructor (props) {
     super(props);
     this.state = {
-      albums: [],
-      selectedAlbum: {}
     };
-    this.selectAlbum = this.selectAlbum.bind(this);
-    this.deselectAlbum = this.deselectAlbum.bind(this);
-  }
-
-  componentDidMount () {
-    axios.get('/api/albums/')
-      .then(res => res.data)
-      .then(albums => {
-        this.setState({ albums })
-      });
-  }
-
-  selectAlbum (albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
-
-  deselectAlbum () {
-    this.setState({ selectedAlbum: {}});
   }
 
   render () {
@@ -43,13 +19,27 @@ export default class Main extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
-        <div className="col-xs-10">
-        {
-          this.state.selectedAlbum.id ?
-          <SingleAlbum album={this.state.selectedAlbum} /> :
-          <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
-        }
-        </div>
+        <Router>
+          <div className="col-xs-10">
+            <Route
+              exact path = "/albums"
+              component = {AllAlbums}>
+            </Route>
+            <Route
+              exact path = "/"
+              component = {AllAlbums}>
+            </Route>
+            <Route
+              path="/albums/:albumId"
+              component = {SingleAlbum}>
+            </Route>
+            {//  this.state.selectedAlbum.id ?
+            //  <SingleAlbum album={this.state.selectedAlbum} /> :
+            //  <AllAlbums albums={this.state.albums} selectAlbum={this.selectAlbum} />
+          }
+
+          </div>
+        </Router>
         <Player />
       </div>
     );
